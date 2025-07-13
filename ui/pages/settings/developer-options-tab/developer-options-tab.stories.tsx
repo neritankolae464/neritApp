090fc65627c13cd1b287a812d68177fce4b561f5
neritankolae464 <147962313+neritankolae464@users.noreply.github.com>
2025-07-13 04:20:24 +0300

    Create developer-options-tab.stories.tsx

---
+++ ui/pages/settings/developer-options-tab/developer-options-tab.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DeveloperOptionsTab from './index';
+
+const DeveloperOptionsTabStory = {
+  title: 'Pages/Settings/DeveloperOptionsTab',
+  component: DeveloperOptionsTab,
+};
+
+export const DefaultStory = () => <DeveloperOptionsTab />;
+
+DefaultStory.storyName = 'Default';
+
+export default DeveloperOptionsTabStory;
